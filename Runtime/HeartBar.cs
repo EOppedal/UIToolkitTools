c74fb1cf9c0da19aaf54029c4ae61e8265f397ed
EOppedal <erlend.oppedal@gmail.com>
2024-09-25 15:44:16 +0200

    rename + add HeartBar

---
+++ Runtime/HeartBar.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[UxmlElement] 
+public partial class HeartBar : VisualElement {
+    private int _heartCount;
+    private Sprite _heartIcon;
+
+    [UxmlAttribute]
+    public int HeartCount {
+        get => _heartCount;
+        set {
+            _heartCount = value;
+            UpdateHearts(_heartCount);
+        }
+    }
+
+    [UxmlAttribute]
+    public Sprite HeartIcon {
+        get => _heartIcon;
+        set {
+            _heartIcon = value;
+            UpdateHearts(HeartCount);
+        }
+    }
+
+    private void UpdateHearts(int heartCount) {
+        if (_heartIcon == null) return;
+        
+        Clear();
+
+        for (var i = 0; i < heartCount; i++) {
+            var heartImage = new VisualElement {
+                style = {
+                    backgroundImage = new StyleBackground(_heartIcon),
+                    marginRight = 1,
+                    width = 15,
+                    height = 12
+                }
+            };
+            Add(heartImage);
+        }
+    }
+}
